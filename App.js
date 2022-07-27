9b867f6070b1ef950eceb88a5f1f607d51f52e6c
hadils <hadilsabbagh18@gmail.com>
2022-07-27 15:49:27 -0700

    ios

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {NativeBaseProvider, Button, VStack} from "native-base";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NativeBaseProvider>
+            <SafeAreaView>
+                <VStack
+                    w={'100%'}
+                    space={4}
+                    px={'2'}
+                    mt={'4'}
+                    alignItems={'center'}
+                    justifyContent={'center'}>
+                    <Button
+                        shadow={2}
+                        size={'lg'}
+                        onPress={() => console.log("Button pressed!")}>Click me!</Button>
+                </VStack>
+            </SafeAreaView>
+        </NativeBaseProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
