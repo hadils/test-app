fca74b3f11ffdb533e8cc9d029d243b080539867
hadils <hadilsabbagh18@gmail.com>
2022-07-27 16:15:46 -0700

    Add Eartho stuff

--- App.js
+++ App.js
@@ -2,8 +2,37 @@ import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import {NativeBaseProvider, Button, VStack} from "native-base";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {init as earthoInit, connectWithRedirect, getIdToken, getUser} from "@eartho/one-client-react-native";
+
+async function logUser() {
+    let user = await getUser();
+
+    console.log(user);
+};
+
+async function logIdToken() {
+    let idToken = await getIdToken();
+
+    console.log(idToken);
+};
 
 export default function App() {
+    React.useEffect(() => {
+
+        earthoInit(
+            "Zw6rVtObnmY0PrSxARkb",
+            "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1bET3c/5H/dQVgVWTZLM\n" +
+            "MgK7ipxKSCSVsskbUhdRhmRIOzOEXPmnE7WWBEAOkKmFhzEgyYeNSKQCDWJ84nRQ\n" +
+            "TWPKXCUfNVdpHbitbXPV1HDxLzKQ+qeQ1ii5Ex/shpi8cpP/eF5GskTNebhJNoHi\n" +
+            "i7hxqLp77Jy0BToHrB017xc0wXrByAIkpPvVmFdRAoklgmuqfaTgo0LjomIgvysy\n" +
+            "jPUD7IpDeuVqrvAc59AjRvewl+Cv9heNoE8FS7Srz5KnkAQvjctBlz57jlzSzL3Y\n" +
+            "3RbkUlW6oC55lilAqZtnZb2B6542rMc9EM9SjVs9ldNwy7Z7tJ6PHD2KZOtKFB6S\n" +
+            "8QIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n");
+
+    }, []);
+
     return (
         <NativeBaseProvider>
             <SafeAreaView>
@@ -17,11 +46,18 @@ export default function App() {
                     <Button
                         shadow={2}
                         size={'lg'}
-                        onPress={() => console.log("Button pressed!")}>Click me!</Button>
+                        onPress={() => connectWithRedirect("lWUJneaRJaCB5HjVJeZt")}>Sign in!</Button>
+                    <Button
+                        shadow={2}
+                        size={'lg'}
+                        onPress={logUser}>Get user in console</Button>
+                    <Button
+                        shadow={2}
+                        size={'lg'}
+                        onPress={logIdToken}>Get idToken in console</Button>
                 </VStack>
             </SafeAreaView>
-        </NativeBaseProvider>
-    );
+        </NativeBaseProvider>);
 }
 
 const styles = StyleSheet.create({
